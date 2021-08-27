0afb890e56f24840c219d1ea836b49f0c1d938d1
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2021-08-27 06:55:40 +0200

    updated readme script to generate index.md for github pages

--- make_readme.sh
+++ make_readme.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # https://jackmckew.dev/make-a-readme-documentation-with-jupyter-notebooks.html
 poetry run jupyter nbconvert --ClearMetadataPreprocessor.enabled=True --ClearOutput.enabled=True --to markdown README.ipynb
+cp README.md index.md
