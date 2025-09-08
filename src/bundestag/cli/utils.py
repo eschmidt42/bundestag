def685d52c82e3c008c4113da49d29d3d1fa3907
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2025-09-08 07:47:03 +0200

    chore: factored out some additional arguments for re-use

--- src/bundestag/cli/utils.py
+++ src/bundestag/cli/utils.py
@@ -1,3 +1,12 @@
 import typer
 
 OPTION_DRY = typer.Option(False, help="Dry or not")
+OPTION_DATA_PATH = typer.Option("data", help="Root dir for data storage")
+ARGUMENT_LEGISLATURE_ID = typer.Argument(
+    111,
+    help="Bundestag legislature id value, see https://www.abgeordnetenwatch.de/bundestag -> Button 'Open Data'",
+)
+OPTION_Y = typer.Option(
+    default=False,
+    help="Assume yes to all prompts and run non-interactively.",
+)
